3cc7fdd24769674420bf0dd69655a4afe2aef6f8
Jan Wilmans <jan.wilmans@vimec.nl>
2024-02-25 01:54:22 +0100

    Fixed apply_low_hanging_fruit.py writing Priority.LOW_HANGING as text in stead of integer value

--- verification/apply_low_hanging_fruit.py
+++ verification/apply_low_hanging_fruit.py
@@ -9,6 +9,7 @@ import util
 from util import Column, Priority
 from util import eprint
 
+low_hanging_issue_count_treshold = 20
 
 def read():
     results = []
@@ -33,7 +34,7 @@ def show_usage():
         eprint("  I got:", sys.argv)
         eprint("")
     eprint("Usage: <input> | " + os.path.basename(__file__))
-    eprint("   re-prioritize rules that have less then 5 issues to top-priority")
+    eprint(f"   re-prioritize rules that have less then {low_hanging_issue_count_treshold} issues to top-priority")
 
 
 def main():
@@ -47,13 +48,13 @@ def main():
     rule_set = set()
     for rule in rules:
         count = rules[rule]
-        if count <= 5:
+        if count <= low_hanging_issue_count_treshold:
             rule_set.add(rule)
 
     for line in lines:
         rule = line[Column.RULE]
         if rule in rule_set:
-            line[Column.PRIO] = str(Priority.LOW_HANGING)
+            line[Column.PRIO] = str(Priority.LOW_HANGING.value)
             line[Column.DESCRIPTION] = "[LOW] " + line[Column.DESCRIPTION]
         util.report_list(line)
 
